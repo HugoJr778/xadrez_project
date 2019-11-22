8f4f53fff545faf5e55ad44a1b441d194a731b42
HugoJr778 <hugo.santos778@outlook.com>
2019-11-22 09:55:06 -0300

    Switching Player Each Turn

--- src/application/UI.java
+++ src/application/UI.java
@@ -3,6 +3,7 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -29,6 +30,13 @@ public class UI {
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	
 	private UI() {}
+	
+	public static void printMatch(ChessMatch chessMatch) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println("Turn: " + chessMatch.getTurn());
+		System.out.println("Waiting Player: " + chessMatch.getCurrentPlayer());
+	}
 	
 	public static void clearScreen() {
 		System.out.print("\033[H\033[2J");
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -9,10 +9,19 @@ import chess.pieces.Rook;
 public class ChessMatch {
 
 	private Board board;
+	private Integer turn;
+	private Color currentPlayer;
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
+		currentPlayer = Color.WHITE;
+		turn = 1;
 		initialSetup();
+	}
+	
+	private void nextTurn() {
+		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
+		turn++;
 	}
 	
 	public ChessPiece[][] getPieces() {
@@ -37,6 +46,7 @@ public class ChessMatch {
 		validateSourcePosition(source);
 		validadeTargetPosition(source, target);
 		Piece capturedPiece = mekaMove(source, target);
+		nextTurn();
 		return (ChessPiece) capturedPiece;
 	}
 	
@@ -56,6 +66,8 @@ public class ChessMatch {
 	private void validateSourcePosition(Position position) {
 		if(!board.thereIsAPiece(position))
 			throw new ChessException("There is no piece on source position");
+		if(currentPlayer != ((ChessPiece) board.piece(position)).getColor()) 
+			throw new ChessException("The chosen piece is not yours");
 		if(!board.piece(position).isThereAnyPossibleMove())
 			throw new ChessException("There is no possible moves for the chosen piece");
 	}
@@ -78,5 +90,12 @@ public class ChessMatch {
         placeNewPiece('e', 7, new Rook(board, Color.BLACK));
         placeNewPiece('e', 8, new Rook(board, Color.BLACK));
         placeNewPiece('d', 8, new King(board, Color.BLACK));
+	}
+
+	public Integer getTurn() {
+		return turn;
+	}
+	public Color getCurrentPlayer() {
+		return currentPlayer;
 	}
 }
